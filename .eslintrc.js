b975feb2d34a4c2c6c9083a59beda91bfafc84cc
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-03-31 20:06:33 -0400

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+      },
+    "plugins": ["prettier"],
+};
