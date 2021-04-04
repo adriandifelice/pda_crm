e0aa4e0017193b2d36dbcc57501ac45888f46ba1
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-04-04 18:12:26 -0400

    controller - get yelp data

--- controllers.js
+++ controllers.js
@@ -1,4 +1,7 @@
 const ClientModel = require('./models/client')
+const axios = require('axios');
+const apikey = process.env.YELP_KEY;
+const ENDPOINT = "/businesses/search"
 
 
 async function getClients(_, res) {
@@ -32,9 +35,28 @@ async function deleteClient(req, res) {
   
 }  
 
+let yelpREST = axios.create({
+  baseURL: "https://api.yelp.com/v3/",
+  headers: {
+    Authorization: `Bearer ${apikey}`,
+    "Content-type": "application/json",
+  },
+})
+
+
+
 async function getYelpData (req, res) {
-console.log('something here 🥖', req.body);
-res.send('response sent ');
+  yelpREST("/businesses/search", {
+    params: {
+     location: "new york",
+     term: req.body,
+     limit: 10,
+   },
+  }).then(({ data }) => {
+   let { businesses } = data;
+   res.send(businesses);
+})
+
 }
 
 
